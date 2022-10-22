90116e1c648987f2710daab1767e517dc0e46c93
omokaroelliot <omokaroelliot@gmail.com>
2022-10-22 07:07:45 +0100

    signup added

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ let port = process.env.PORT;
 if (port == null || port ==""){
     port == 3000;
 }
- app.listen(port,function(){
-    console.log("Server has  started successful on port 5000")
+ app.listen(3000,function(){
+    console.log("Server has  started successful on port 3000")
 }) 
