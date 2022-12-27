45db7f5adb0507bee47777c4f8f6978fcd0e346f
omokaroelliot <omokaroelliot@gmail.com>
2022-12-28 00:22:11 +0100

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const _ = require("lodash")
 const ejs=require("ejs");
 const app= express();
+let posts = [];
 const mongoose =require("mongoose")
 mongoose.connect("mongodb+srv://Elliotazzurri:england123@cluster0.reotwsp.mongodb.net/peoplesDB",{useNewUrlParser:true})
 var validateEmail = function(email) {
@@ -30,14 +31,18 @@ const peoplesSchema = ({
 })
 const People = mongoose.model("People", peoplesSchema )
  app.set('view engine', 'ejs');
-
+/* Blog articles content */
+const homecontent = "CREATING A BETTER YOU"
+const aboutp ="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, labore reiciendis. Assumenda eos quod animi! Soluta nesciunt inventore dolores excepturi provident, culpa beatae tempora, explicabo corporis quibusdam corrupti. Autem, quaerat. Assumenda quo aliquam vel, nostrum explicabo ipsum dolor, ipsa perferendis porro doloribus obcaecati placeat natus iste odio est non earum?"
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
 app.get("/",function(req,res){
-    res.render("index")
+    res.render("index",{startcontent:homecontent})
 })
 app.get("/about",function(req,res){
-    res.render("about")
+    res.render("about", {aboutcontent:aboutp,
+    posts:posts
+    })
 })
 app.get("/portfolio",function(req,res){
     res.render("portfolio")
@@ -51,6 +56,17 @@ app.get("/success",function(req,res){
 })
 app.get("/failure", function(req,res){
     res.render("failure")
+})
+app.get("/compose", function(req,res){
+    res.render("compose", {})
+})
+app.post("/compose",function(req,res){
+    var post ={
+        title:req.body.posttitle,
+        content:req.body.postcontent
+    }
+    posts.push(post)
+    res.redirect("about")
 })
 app.post("/Signup",function(req,res){
    let username = req.body.user
